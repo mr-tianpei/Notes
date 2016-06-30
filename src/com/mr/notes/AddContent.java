253d23ef0cddea29d3dd3dc2d669bb318d5ab598
mr-tianpei <1453302680@qq.com>
2016-06-30 23:15:57 +0800

    notes添加图片功能完成

--- src/com/mr/notes/AddContent.java
+++ src/com/mr/notes/AddContent.java
@@ -1,12 +1,19 @@
 package com.mr.notes;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,12 +23,14 @@ import android.widget.VideoView;
 
 public class AddContent extends Activity implements OnClickListener {
 	private String val;
-	private Button saveBtn,cancelBtn;
+	private Button saveBtn, cancelBtn;
 	private TextView c_text;
 	private ImageView c_image;
 	private VideoView c_video;
 	private NoteDB noteDB;
 	private SQLiteDatabase dbWriter;
+	private File imgPath,videoPath;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,13 +41,39 @@ public class AddContent extends Activity implements OnClickListener {
 		c_text = (TextView) findViewById(R.id.c_text);
 		c_image = (ImageView) findViewById(R.id.c_image);
 		c_video = (VideoView) findViewById(R.id.c_video);
-		
+
 		saveBtn.setOnClickListener(this);
 		cancelBtn.setOnClickListener(this);
 		noteDB = new NoteDB(this);
 		dbWriter = noteDB.getWritableDatabase();
-		
+		init();
 	}
+
+	public void init() {
+		if (val.equals("1")) {
+			c_image.setVisibility(View.GONE);
+			c_video.setVisibility(View.GONE);
+		}
+		if (val.equals("2")) {
+			c_image.setVisibility(View.VISIBLE);
+			c_video.setVisibility(View.GONE);
+			Intent iimage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			imgPath = new File(Environment.getExternalStorageDirectory()
+					.getAbsoluteFile() + "/DCIM/" + getTime() + ".jpg");
+			iimage.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imgPath));
+			startActivityForResult(iimage, 1);
+		}
+		if (val.equals("3")) {
+			c_image.setVisibility(View.GONE);
+			c_video.setVisibility(View.VISIBLE);
+			Intent ivideo = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			videoPath = new File(Environment.getExternalStorageDirectory()
+					.getAbsoluteFile() + "/DCIM/" + getTime() + ".mp4");
+			ivideo.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(videoPath));
+			startActivityForResult(ivideo, 2);
+		}
+	}
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -52,13 +87,30 @@ public class AddContent extends Activity implements OnClickListener {
 			break;
 		}
 	}
-	private void addContent(){
+
+	private void addContent() {
 		ContentValues values = new ContentValues();
 		values.put(NoteDB.CONTENT, c_text.getText().toString());
 		values.put(NoteDB.TIME, getTime());
+		values.put(NoteDB.PATH, imgPath + "");
+		values.put(NoteDB.VIDEO, videoPath+"");
 		dbWriter.insert(NoteDB.TABLE_NAME, null, values);
 	}
-	public String getTime(){
+
+	public String getTime() {
 		return (new SimpleDateFormat("yyyyƒÍMM‘¬ss»’ HH:mm:ss").format(new Date()));
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == 1) {
+			Bitmap bitmap = BitmapFactory.decodeFile(imgPath.getAbsolutePath());
+			c_image.setImageBitmap(bitmap);
+		}
+		if(requestCode == 2){
+			c_video.setVideoURI(Uri.fromFile(videoPath));
+			c_video.start();
+		}
 	}
 }
--- src/com/mr/notes/MyAdapter.java
+++ src/com/mr/notes/MyAdapter.java
@@ -4,6 +4,10 @@ import com.mr.notes.R;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,9 +55,46 @@ public class MyAdapter extends BaseAdapter {
 		String content = cursor
 				.getString(cursor.getColumnIndex(NoteDB.CONTENT));
 		String time = cursor.getString(cursor.getColumnIndex(NoteDB.TIME));
+		String url = cursor.getString(cursor.getColumnIndex(NoteDB.PATH));
+		String videoUri = cursor.getString(cursor.getColumnIndex(NoteDB.VIDEO));
+		imageViewtv.setImageBitmap(getImageThumbnail(url, 200, 200));
+		videotv.setImageBitmap(getVideoBitmapThumbnail(videoUri, 200, 200, MediaStore.Images.Thumbnails.MICRO_KIND));
 		contenttv.setText(content);
 		timetv.setText(time);
 		return layout;
+	}
+//ªÒ»°Õº∆¨Àı¬‘Õº
+	public Bitmap getImageThumbnail(String uri, int width, int height) {
+		Bitmap bitmap = null;
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		bitmap = BitmapFactory.decodeFile(uri, options);
+		options.inJustDecodeBounds = false;
+		int beHeight = options.outHeight / height;
+		int beWidth = options.outWidth / width;
+		int be = 1;
+		if (beWidth < beHeight) {
+			be = beWidth;
+		} else {
+			be = beHeight;
+		}
+		if (be <= 0) {
+			be = 1;
+		}
+		options.inSampleSize = be;
+		bitmap = BitmapFactory.decodeFile(uri, options);
+		bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
+				ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+		return bitmap;
+	}
+	//ªÒ»° ”∆µÀı¬‘Õº
+	public Bitmap getVideoBitmapThumbnail(String uri,int width,int height,int kind){
+		Bitmap bitmap = null;
+		bitmap = ThumbnailUtils.createVideoThumbnail(uri, kind);
+		bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+		
+		return bitmap;
+		
 	}
 
 }
